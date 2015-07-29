26d362232b67bb6a22b681cc6fb2f10f15a52367
Anthony Cowley <acowley@gmail.com>
2015-07-29 16:38:37 -0400

    GHC 7.10.2

--- default.nix
+++ default.nix
@@ -3,12 +3,13 @@ rec {
   ghc = rec {
     "7.8.4-binary" = import ghc/7.8.4-binary.nix;
     "7.10.1-binary" = import ghc/7.10.1-binary.nix;
+    "7.10.2-binary" = import ghc/7.10.2-binary.nix;
   };
   "cabal-install" = {
     "1.20.0.6" = import cabal/1.20.0.6.nix;
     "1.22.3.0" = import cabal/1.22.3.0.nix;
     "1.22.4.0" = import cabal/1.22.4.0.nix;
   };
-  ghcDefault = callPackage ghc."7.10.1-binary" { libiconv = darwin.libiconv; };
+  ghcDefault = callPackage ghc."7.10.2-binary" { libiconv = darwin.libiconv; };
   cabalDefault = callPackage cabal-install."1.22.4.0" { ghc = ghcDefault; };
 }
